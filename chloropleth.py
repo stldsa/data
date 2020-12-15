0f880627aaced1b2e5deba6a844413c5b867de62
Tyler Schlichenmeyer <tyler.schlichenmeyer@gmail.com>
2020-12-15 17:00:07 -0600

    formatting changes

--- chloropleth.py
+++ chloropleth.py
@@ -7,38 +7,53 @@ import plotly.express as px
 import pandas as pd
 import json
 
-df = pd.read_csv("data/elections/2020-general-election.csv", dtype={"Ward": str})
-with open('data/geojson/stl-city/wards.json') as read_file:
-	wards_geojson = json.load(read_file)
+df = pd.read_csv("data/tabular_data/2020-general-election.csv", dtype={"Ward": str})
+with open("data/geodata/WARDS_2010.json") as read_file:
+    wards_geojson = json.load(read_file)
 
 # TODO: Figure out a better way to handle this candidates stuff, dynamically and by election, ideally
-candidates = ["Trump", "Biden", "Prop D Yes", "Prop D No", "Prop R Yes", "Prop R No", "Prop 1 Yes", "Prop 1 No"]
+candidates = [
+    "Trump",
+    "Biden",
+    "Prop D Yes",
+    "Prop D No",
+    "Prop R Yes",
+    "Prop R No",
+    "Prop 1 Yes",
+    "Prop 1 No",
+]
 
 app = dash.Dash(__name__)
 
-app.layout = html.Div([
-    html.P("Candidate:"),
-    dcc.RadioItems(
-        id='candidate', 
-        options=[{'value': x, 'label': x} 
-                 for x in candidates],
-        value=candidates[0],
-        labelStyle={'display': 'inline-block'}
-    ),
-    dcc.Graph(id="choropleth", style={'height': '80vh'}),
-])
+app.layout = html.Div(
+    [
+        html.P("Candidate:"),
+        dcc.RadioItems(
+            id="candidate",
+            options=[{"value": x, "label": x} for x in candidates],
+            value=candidates[0],
+            labelStyle={"display": "inline-block"},
+        ),
+        dcc.Graph(id="choropleth", style={"height": "80vh"}),
+    ]
+)
 
-@app.callback(
-    Output("choropleth", "figure"), 
-    [Input("candidate", "value")])
+
+@app.callback(Output("choropleth", "figure"), [Input("candidate", "value")])
 def display_choropleth(candidate):
     fig = px.choropleth(
-        df, geojson=wards_geojson, color=candidate,
-        locations="Ward", featureidkey="properties.Ward",
-        projection="mercator", range_color=[0, 6500])
+        df,
+        geojson=wards_geojson,
+        color=candidate,
+        locations="Ward",
+        featureidkey="properties.Ward",
+        projection="mercator",
+        range_color=[0, 6500],
+    )
     fig.update_geos(fitbounds="locations", visible=False)
-    fig.update_layout(margin={"r":0,"t":0,"l":0,"b":0})
+    fig.update_layout(margin={"r": 0, "t": 0, "l": 0, "b": 0})
 
     return fig
+
 
 app.run_server(debug=True)
